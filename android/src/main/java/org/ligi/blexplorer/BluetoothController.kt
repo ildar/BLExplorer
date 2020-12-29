b0cafa6510575b6b3139e1e647f8bb70c1150d7e
Sandeep Raghuraman <sandy.8925@gmail.com>
2020-12-29 06:56:15 +0530

    Add RxBle library, use it to scan for devices

--- android/src/main/java/org/ligi/blexplorer/BluetoothController.kt
+++ android/src/main/java/org/ligi/blexplorer/BluetoothController.kt
@@ -4,12 +4,19 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.util.Log
 import androidx.collection.ArrayMap
 import androidx.lifecycle.LiveData
+import com.polidea.rxandroidble2.RxBleClient
+import com.polidea.rxandroidble2.scan.ScanResult
+import com.polidea.rxandroidble2.scan.ScanSettings
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
 import org.ligi.blexplorer.scan.DeviceInfo
 
 internal class BluetoothController(context: Context) {
     private val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    internal val rxBleClient = RxBleClient.create(context)
 
     fun isBluetoothAvailable() = bluetoothManager.adapter != null
 
@@ -23,21 +30,28 @@ internal class BluetoothController(context: Context) {
     internal val deviceListLiveData : LiveData<List<DeviceInfo>> = DeviceListLiveData()
 }
 
-private class DeviceListLiveData : LiveData<List<DeviceInfo>>(), BluetoothAdapter.LeScanCallback {
+private class DeviceListLiveData : LiveData<List<DeviceInfo>>() {
+    private val errorHandler: Consumer<in Throwable> = Consumer {
+        Log.e("bluetooth_scan", "Exception occurred while scanning for BLE devices", it)
+    }
+
+    private val scanResultHandler: Consumer<in ScanResult> = Consumer {
+        val device = it.bleDevice.bluetoothDevice
+        devices[device] = DeviceInfo(device, it.rssi, it.scanRecord.bytes)
+        postValue(devices.values.toList())
+    }
+
     private val devices: MutableMap<BluetoothDevice, DeviceInfo> = ArrayMap()
+    private var disposable : Disposable? = null
 
     override fun onActive() {
         super.onActive()
-        bluetoothController.bluetoothAdapter()?.startLeScan( this)
+        disposable = bluetoothController.rxBleClient.scanBleDevices(ScanSettings.Builder().build())
+                .subscribe(scanResultHandler, errorHandler)
     }
 
     override fun onInactive() {
         super.onInactive()
-        bluetoothController.bluetoothAdapter()?.stopLeScan(this)
-    }
-
-    override fun onLeScan(device: BluetoothDevice, rssi: Int, scanRecord: ByteArray) {
-        devices[device] = DeviceInfo(device, rssi, scanRecord)
-        postValue(devices.values.toList())
+        disposable?.dispose()
     }
 }
