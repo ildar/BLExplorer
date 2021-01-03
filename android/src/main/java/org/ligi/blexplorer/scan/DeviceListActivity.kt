b06f980f75301593dd4bcc6ec3b0a24e1889c742
Sandeep Raghuraman <sandy.8925@gmail.com>
2021-01-04 05:07:27 +0530

    DeviceListActivity: Remove check for Bluetooth feature, since we have marked Bluetooth as required in the manifest

--- android/src/main/java/org/ligi/blexplorer/scan/DeviceListActivity.kt
+++ android/src/main/java/org/ligi/blexplorer/scan/DeviceListActivity.kt
@@ -1,9 +1,7 @@
 package org.ligi.blexplorer.scan
 
-import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
@@ -31,7 +29,6 @@ import org.ligi.blexplorer.util.ManufacturerRecordParserFactory
 import org.ligi.blexplorer.util.from_lollipop.ScanRecord
 import org.ligi.tracedroid.sending.TraceDroidEmailSender
 import java.math.BigInteger
-import java.util.*
 
 class DeviceListActivity : AppCompatActivity() {
 
@@ -71,14 +68,7 @@ class DeviceListActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        if (!bluetoothController.isBluetoothAvailable()) {
-            AlertDialog.Builder(this)
-                    .setMessage(R.string.bluetooth_needed_error_msg)
-                    .setTitle(R.string.error)
-                    .setPositiveButton(R.string.exit) { _: DialogInterface, _: Int ->
-                this@DeviceListActivity.finish()
-            }.show()
-        } else if (!bluetoothController.isBluetoothEnabled()) {
+        if (!bluetoothController.isBluetoothEnabled()) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
         }
