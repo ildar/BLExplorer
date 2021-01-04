9d36e1b1bcebe45f23547e2f5440e934e13da90b
Sandeep Raghuraman <sandy.8925@gmail.com>
2021-01-04 05:57:48 +0530

    Code cleanup

--- android/src/main/java/org/ligi/blexplorer/BluetoothController.kt
+++ android/src/main/java/org/ligi/blexplorer/BluetoothController.kt
@@ -1,6 +1,5 @@
 package org.ligi.blexplorer
 
-import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Context
@@ -19,14 +18,10 @@ internal class BluetoothController(context: Context) {
     private val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
     internal val rxBleClient = RxBleClient.create(context)
 
-    fun isBluetoothAvailable() = bluetoothManager.adapter != null
-
     fun isBluetoothEnabled() : Boolean {
         bluetoothManager.adapter ?: return false
         return bluetoothManager.adapter.isEnabled
     }
-
-    internal fun bluetoothAdapter() : BluetoothAdapter? = bluetoothManager.adapter
 
     internal fun deviceListLiveData() : LiveData<List<DeviceInfo>> = DeviceListLiveData()
 
